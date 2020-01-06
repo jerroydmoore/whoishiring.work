04cceb8ce38d9955612a412b8a12d7f0adcd1cb8
Jerroyd Moore <jerroydmoore@gmail.com>
2020-01-05 23:23:39 -0800

    feat(frontend): users can add notes to posts

--- frontend/src/components/jobPost/index.js
+++ frontend/src/components/jobPost/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
-import BooleanUserAction from './BooleanUserAction';
+import { Accordion, Card, Button, Form } from 'react-bootstrap';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as far from '@fortawesome/free-regular-svg-icons';
 import { Markup } from 'interweave';
+
+import BooleanUserAction from './BooleanUserAction';
 
 import { update as updateUserPost } from '../../services/userPost';
 
@@ -11,20 +13,35 @@ const JobPost = ({ job }) => {
   console.log('JobPost render ' + JSON.stringify(job.userPosts));
 
   const updateUserPostField = (field, value) => updateUserPost(job.id, field, value);
-
+  const onNotesUpdate = ({ target }) => updateUserPost(job.id, 'notes', target.value);
   return (
     <Card as="section" className="job-posting">
-      <Card.Body className="post-actionbar">
-        <BooleanUserAction
-          field="applied"
-          value={job.userPosts.applied}
-          trueIcon={far.faCheckSquare}
-          falseIcon={far.faSquare}
-          trueLabel="Un-Mark as Applied"
-          falseLabel="Mark as Applied"
-          onClick={updateUserPostField}
-        />
-      </Card.Body>
+      <Accordion defaultActiveKey={job.userPosts.notes && '0'}>
+        <Card.Body className="post-actionbar">
+          <Accordion.Toggle as={Button} variant="light" className="card-link" eventKey="0">
+            <FontAwesomeIcon icon={far.faStickyNote} /> Notes
+          </Accordion.Toggle>
+          <BooleanUserAction
+            field="applied"
+            value={job.userPosts.applied}
+            trueIcon={far.faCheckSquare}
+            falseIcon={far.faSquare}
+            trueLabel="Un-Mark as Applied"
+            falseLabel="Mark as Applied"
+            onClick={updateUserPostField}
+          />
+        </Card.Body>
+
+        <Accordion.Collapse className="user-notes" eventKey="0">
+          <Form.Control
+            onChange={onNotesUpdate}
+            defaultValue={job.userPosts.notes || ''}
+            as="textarea"
+            rows="3"
+            placeholder="Type some notes here. Notes are automatically saved. Notes are private only to you."
+          />
+        </Accordion.Collapse>
+      </Accordion>
       <Card.Body>
         <Card.Title>
           <Markup content={job.title} />
