eb916bc403302ca61a23828d2bf2c28d8b28a050
Jerroyd Moore <jerroydmoore@gmail.com>
2020-03-27 13:51:10 -0700

    feat(tf): Add populate_hn_data lambda function

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,15 +5,6 @@ const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./app');
 const server = awsServerlessExpress.createServer(app);
 
-exports.handler = (event, context) => {
+exports.main = (event, context) => {
   awsServerlessExpress.proxy(server, event, context);
 };
-
-exports.handler_test = (event, context) => ({
-  statusCode: 200,
-  body: JSON.stringify({
-    message: 'hello world',
-    event,
-    context,
-  }),
-});
--- terraform/services.tf
+++ terraform/services.tf
@@ -19,3 +19,24 @@ module "backend" {
   }
 }
 
+module "populate_hn_data" {
+  source = "./modules/upload_lambda_function"
+
+  name        = "populate-hn-data"
+  prefix      = var.application_id
+  description = "Pulls data from Hacker News"
+  source_dir  = "../populate-hn-data"
+  handler     = "src/index.main"
+
+  environment = {
+    HOWMANY_STORIES = var.howmany_stories
+    PGUSER          = var.db_user
+    PGPASSWORD      = var.db_user_password
+    PGDATABASE      = var.db_name
+    PGHOST          = aws_db_instance.rds.address
+    PGPORT          = aws_db_instance.rds.port
+  }
+  tags = {
+    Application = var.application_id
+  }
+}
